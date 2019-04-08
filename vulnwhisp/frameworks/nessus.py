c8d906c05fa752a5c2df1f596de91c2a859bc89b
pemontto <pemontto@gmail.com>
2019-04-08 19:30:48 +1000

    Fix tenable downloads

--- vulnwhisp/frameworks/nessus.py
+++ vulnwhisp/frameworks/nessus.py
@@ -144,7 +144,7 @@ class NessusAPI(object):
                 self.logger.info("Completed: {}".format(counter))
         self.logger.info("Done: {}".format(counter))
         if profile == 'tenable':
-            content = self.request(self.EXPORT_FILE_DOWNLOAD.format(file_id=file_id), method='GET', download=True)
+            content = self.request(self.EXPORT_FILE_DOWNLOAD.format(scan_id=scan_id, file_id=file_id), method='GET', download=True)
         else:
             content = self.request(self.EXPORT_TOKEN_DOWNLOAD.format(token_id=token_id), method='GET', download=True)
         return content
