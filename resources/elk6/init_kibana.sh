00f9b7659b53898788ad08fde88f813f460618eb
pemontto <pemontto@gmail.com>
2019-04-15 18:17:43 +1000

    Docker install ES index template

--- resources/elk6/init_kibana.sh
+++ resources/elk6/init_kibana.sh
@@ -2,13 +2,23 @@
 
 #kibana_url="localhost:5601"
 kibana_url="kibana.local:5601"
+elasticsearch_url="elasticsearch.local:9200"
 add_saved_objects="curl -u elastic:changeme -k -XPOST 'http://"$kibana_url"/api/saved_objects/_bulk_create' -H 'Content-Type: application/json' -H \"kbn-xsrf: true\" -d @"
 
 #Create all saved objects - including index pattern
 saved_objects_file="kibana_APIonly.json"
 
 #if [ `curl -I localhost:5601/status | head -n1 |cut -d$' ' -f2` -eq '200' ]; then echo "Loading VulnWhisperer Saved Objects"; eval $(echo $add_saved_objects$saved_objects_file); else echo "waiting for kibana"; fi
- 
+
+until curl -s "$elasticsearch_url/_cluster/health?pretty" | grep '"status"' | grep -q green; do
+    curl -s "$elasticsearch_url/_cluster/health?pretty"
+    echo "Waiting for Elasticsearch"
+    sleep 5
+done
+
+echo "Loading VulnWhisperer index template"
+curl -XPUT "http://$elasticsearch_url/_template/vulnwhisperer" -H 'Content-Type: application/json' -d '@/opt/index-template.json'
+
 until [ "`curl -I "$kibana_url"/status | head -n1 |cut -d$' ' -f2`" == "200" ]; do
     curl -I "$kibana_url"/status
     echo "Waiting for Kibana"
