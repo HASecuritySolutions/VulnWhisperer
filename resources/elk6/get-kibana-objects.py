1597ad13e80bf545e7c7511c9719c67fea846f9e
pemontto <pemontto@gmail.com>
2019-05-02 11:42:41 +0100

    add utility to extract kibana objects

---
+++ resources/elk6/get-kibana-objects.py
@@ -0,0 +1,15 @@
+import json
+import requests
+
+api_objects = []
+
+for object_type in ['dashboard', 'visualization', 'search', 'index-pattern', 'timelion-sheet']:
+    r = requests.get('http://localhost:5601/api/saved_objects/_find?per_page=500&type={}'.format(object_type)).json()
+    api_objects += r['saved_objects']
+    print object_type, len(r['saved_objects'])
+    print len(api_objects)
+
+for api_object in api_objects:
+    api_object.pop('updated_at', None)
+
+json.dump(api_objects, open('kibana_APIonly.json', 'w'), indent=2)
